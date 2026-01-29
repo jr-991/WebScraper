1d08b9ec109b0d1022a34702bda7f5e603311586
Juan <juan.rivaldo@connect.qut.edu.au>
2026-01-29 21:51:52 +1000

    Set up main file

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+def main():
+    print("Hello, World!")
+
+
+def GUI():
+    root = tk.Tk()
+    root.title("Scraper GUI")
+    root.geometry("400x300")
+
+
+
+    root.mainloop()
+    pass
+
+if __name__ == "__main__":
+    main()
+    GUI()
