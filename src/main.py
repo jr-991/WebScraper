c8c3c417b442e0aa45f4a5879e1d0a6dbb707b40
Juan <juan.rivaldo@connect.qut.edu.au>
2026-01-30 18:35:02 +1000

    Configured main layout (displays and UI elements)

--- src/main.py
+++ src/main.py
@@ -7,12 +7,26 @@ def main():
 def GUI():
     root = tk.Tk()
     root.title("Scraper GUI")
-    root.geometry("400x300")
 
+    frame = tk.Frame(root, width=1280, height=720)
+    frame.pack(padx=10, pady=10)    
 
+    topFrame = tk.Frame(frame)
+    topFrame.pack(pady=10)
 
+    label = tk.Label(topFrame, text="Pull latest: ", font=("Arial", 16))
+    label.pack(side='left', padx=(0, 5))
+
+    pullButton = tk.Button(topFrame, text = "↓", command = Scrape)
+    pullButton.pack(side='left')
+    
+    dataFrame = tk.Frame(frame, width=600, height=400, bg="lightgrey")
+    dataFrame.pack(pady=10, padx=10)
+    
     root.mainloop()
-    pass
+
+def Scrape():
+    print("Scraping data...")
 
 if __name__ == "__main__":
     main()
