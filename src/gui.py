d99d9d7e229f8905e26a6db609d325770ae08974
Juan <juan.rivaldo@connect.qut.edu.au>
2026-09-03 23:09:49 +1000

    - Added API foundation for data collection/manipulation
    - Improved flexibility and adaptability of current GUI
    
    - Restructured project to facilitate future expansion

---
+++ src/gui.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+
+def GUI():
+    root = tk.Tk()
+    root.title("Real Estate Viewer")
+    root.geometry("1280x720")
+
+    frame = tk.Frame(root, width=1280, height=720)
+    frame.pack(padx=10, pady=10)    
+
+    topFrame = tk.Frame(frame)
+    topFrame.pack(pady=10)
+
+    label = tk.Label(topFrame, text="Pull latest: ", font=("Arial", 16))
+    label.pack(side='left', padx=(0, 5))
+
+    pullButton = tk.Button(topFrame, text = "↓", command = Scrape)
+    pullButton.pack(side='left')
+    
+    dataFrame = tk.Frame(frame, bg="lightgrey", width=1280, height=720)
+    dataFrame.pack(fill='both', expand=True, pady=10, padx=10)
+    
+    root.mainloop()
+
+def Scrape():
+    print("Fetching data...")
+
+if __name__ == "__main__":
+    GUI()
--- src/main.py
+++ src/main.py
@@ -1,28 +1,7 @@
-import tkinter as tk
+from fastapi import FastAPI
 
-def GUI():
-    root = tk.Tk()
-    root.title("Scraper GUI")
+app = FastAPI(title="Real Estate API", description="API for managing real estate listings", version="1.0.0")
 
-    frame = tk.Frame(root, width=1280, height=720)
-    frame.pack(padx=10, pady=10)    
-
-    topFrame = tk.Frame(frame)
-    topFrame.pack(pady=10)
-
-    label = tk.Label(topFrame, text="Pull latest: ", font=("Arial", 16))
-    label.pack(side='left', padx=(0, 5))
-
-    pullButton = tk.Button(topFrame, text = "↓", command = Scrape)
-    pullButton.pack(side='left')
-    
-    dataFrame = tk.Frame(frame, width=600, height=400, bg="lightgrey")
-    dataFrame.pack(pady=10, padx=10)
-    
-    root.mainloop()
-
-def Scrape():
-    print("Scraping data...")
-
-if __name__ == "__main__":
-    GUI()
+@app.get("/")
+def read_root():
+    return {"message": "The Real Estate API is running"}
